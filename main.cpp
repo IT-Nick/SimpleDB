beae19934d305a488cab7402761b021958e7ec11
Stepan <stepan-pl@protonmail.com>
2022-03-02 06:49:22 +0300

    Addition map, set and bit changes

--- main.cpp
+++ main.cpp
@@ -1,22 +1,61 @@
 #include <iostream>
+#include <sstream>
+#include <map>
+#include <set>
 
 class Date {
-    int GetYear() const;
-    int GetMounth() const;
-    int GetDay() const;
+public:
+    Date(std::string date) {
+        std::stringstream ss;
+        ss << date;
+        ss >> year;
+        ss.ignore(1);
+        ss >> month;
+        ss.ignore(1);
+        ss >> day;
+    }
+    int GetYear() const {
+        return year;
+    }
+    int GetMonth() const {
+        return month;
+    }
+    int GetDay() const {
+        return day;
+    }
+private:
+    int year;
+    int month;
+    int day;
 };
 
-bool operator < (const Date& lhs, const Date& rhs);
+bool operator < (const Date& lhs, const Date& rhs) {
+    return lhs.GetYear() < rhs.GetYear();
+}
+
+std::ostream& operator << (std::ostream& os, const std::set<std::string>& s) {
+    for(auto it = s.begin(); it != s.end(); ++it) {
+        os << *it;
+    }
+    return os;
+}
+
 
 class Database {
 public:
-    void AddEvent(const Date& date, const std::string& event);
+    void AddEvent(const Date& date, const std::string& event) {
+        db[date].insert(event);
+    }
     bool DeleteEvent(const Date& date, const std::string& event);
     int DeleteDate(const Date& date);
 
     void Find(const Date& date) const;
 
-    void Print() const;
+    std::map<Date, std::set<std::string>> Print() const {
+        return db;
+    }
+private:
+    std::map<Date, std::set<std::string>> db;
 };
 
 enum class RequestEvents {
@@ -37,8 +76,10 @@ int main() {
         switch (request) {
             //добавление события
             case RequestEvents::ADD: {
-                int date, events;
-                std::cin >> date >> events;
+                std::string input, event;
+                std::cin >> input >> event;
+                Date date(input);
+                db.AddEvent(date, event);
                 break;
             }
                 //удаление события
@@ -55,7 +96,13 @@ int main() {
             }
                 //печать всех событий за все даты
             case RequestEvents::PRINT: {
-
+                std::map<Date, std::set<std::string>> s = db.Print();
+                for(auto it = s.begin(); it != s.end(); ++it) {
+                    std::cout << it->first.GetYear() << " "
+                    << it->first.GetMonth() << " "
+                    << it->first.GetDay() << " "
+                    << it->second << " ";
+                }
                 break;
             }
         }
